8fe1c97e8cd7f7503b6574912048f9383568239c
Allen <leander_lightlord99@hotmail.com>
2023-05-19 22:30:31 +0200

    Error messages with js

--- account.php
+++ account.php
@@ -1,6 +1,5 @@
 <?php 
-  session_start(); 
-  include "config/uniCRUD.config.php";
+  session_start();
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +16,7 @@
     <!-- Javascript -->
     <script defer src="javascript/popup.window.js"></script>
     <script defer src="javascript/clear.fields.js"></script>
+    <script defer src="javascript/messages.js"></script>
   </head>
   <body>
     <header>
@@ -138,47 +138,21 @@
           <button class="alt" onclick="ClearPersFields();">Clear</button>
         </div>
       </div>
-      <!--<div class="dashboard">
-        <form class="window-body" id="sheet" method="post">
-          <div class="nest">
-            <span>Personal Information</span>
-            <label for="firstname">Firstname</label>
-            <input type="text" name="firstname" placeholder="Firstname">
-            <label for="lastname">Lastname</label>
-            <input type="text" name="lastname" placeholder="Lastname">
+
+      <!--<div class="window" id="window">
+        <div class="window-title">
+          <div class="title">We hate to see you leave...</div>
+          <button data-window-close class="close-button">&#215;</button>
+        </div>
+        <form class="window-body" name="login-form" action="config/logout.config.php" method="post">
+          <span class="error">Warning: These changes cannot be undone!<br>Any information will be removed from our servers</span>
+          <span class="error-uid"></span>
+          <span class="error-pwd"></span>
+          <label for="cv-name">Do you really want to delete your account?</label>
+          <input type="text" name="cv-name" placeholder="Confirm your password for the last time." required>
+          <div class="window-title">
+            <button type="submit" class="Del" type="submit" name="delete">Delete Account</button>
           </div>
-          <div class="nest">
-            <span>Account Information</span>
-            <label for="username">Username</label>
-            <input type="text" name="username" placeholder="Username">
-            <label for="email">Email</label>
-            <input type="text" name="email" placeholder="Email Address">
-          </div>
-          <div class="nest">
-            <label for="nationality">Nationality</label>
-            <input type="text" name="nationality" placeholder="Nationality">
-            <label for="birth">Date of Birth</label>
-            <input type="date" name="birth" placeholder="Birthday">
-          </div>
-          <div class="nest">
-            <label for="pwd">Password</label>
-            <input type="password" name="pwd" placeholder="Password">
-            <label for="pwd">Repeat your Password</label>
-            <input type="password" name="pwdR" placeholder="Password">
-          </div>
-          <div class="nest">
-            <label for="streetname">Street</label>
-            <input type="text" name="streetname" placeholder="Streetname">
-            <label for="city">Town / City</label>
-            <input type="text" name="city" placeholder="Hometown or City">
-          </div>
-          <div class="nest">
-            <label for="postalcode">Postalcode</label>
-            <input type="text" name="postalcode" placeholder="Postalcode">
-            <label for="phone">Mobile Number</label>
-            <input type="text" name="phone" placeholder="Mobile Number">
-          </div>
-          <button type="submit" name="submit">Save Changes</button>
         </form>
       </div>-->
 
@@ -187,13 +161,16 @@
           <div class="title">We hate to see you leave...</div>
           <button data-window-close class="close-button">&#215;</button>
         </div>
-        <form class="window-body" action="config/logout.config.php" method="post">
-          <ul id='form-message'>Warning: These changes cannot be undone!<br>Any information will be removed from our servers</ul>
-          <label for="cv-name">Do you really want to delete your account?</label>
-          <input type="text" name="cv-name" placeholder="Please enter your password for confirmation" required>
-          <div class="window-title">
-            <button type="submit" class="close-button" type="submit" name="delete">Confirm</button>
-          </div>
+        <form class="window-body" name="login-form" action="config/destroy.config.php" method="post">
+          <p class="error-message">Warning: These changes cannot be undone!<br> Any information will be removed from our servers</p>
+          <span class="error-uid"></span>
+          <span class="error-pwd"></span>
+          <label>Do you really want to delete your account?</label>
+          <label for="username"></label>
+          <input type="text" name="username" placeholder="Enter your username or email" id="user" autocomplete="off">
+          <label for="pwd"></label>
+          <input type="password" name="pwd" id="pass" placeholder="Confirm your password for the last time">
+          <button type="submit" class="Del" type="submit" name="delete">Delete Account</button>
         </form>
       </div>
       <div id="overlay"></div>
