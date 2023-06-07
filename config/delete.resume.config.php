6216141a1a3c6a435cefea2a4ebfbecf56f5cf94
loubna faress <9008427@student.zadkine.nl>
2023-06-07 11:11:56 +0200

    DELETE RESUME IS AFFFFFF!!!!

--- config/delete.resume.config.php
+++ config/delete.resume.config.php
@@ -22,7 +22,7 @@
         if (isset($_POST['user_id'])) {
 
           // Select the record that is to be 'deleteResume'
-          $stmt = $this->pdo->prepare('SELECT *  FROM accounts WHERE userID = ?');
+          $stmt = $this->pdo->prepare('SELECT *  FROM `resume` WHERE userID = ?');
           $stmt->execute([$_POST['user_id']]);
           $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -36,26 +36,20 @@
           // Make sure the user has confirmed before deletion
           if (isset($_POST['delete'])) {
 
-              // Erase data from Accounts
-              $stmt = $this->pdo->prepare('DELETE FROM accounts WHERE userID = ?');
+              // Erase data from resume
+              $stmt = $this->pdo->prepare('DELETE FROM `resume` WHERE userID = ?');
               $stmt->execute([$_POST['user_id']]);
               $stmt = null;
 
               // Queries voor de andere tabellen zijn niet meer nodig dankzij het volgende.
                     // Buiten accounts, heeft nu elk tabel de foreign key: ON DELETE & UPDATE (van userID) CASCADE.
 
-              // Reset auto increment in Accounts from the current highest userID.
-              $stmt = $this->pdo->prepare('SELECT MAX(`userID`) FROM `accounts`;');
+              // Reset auto increment in resume from the current highest userID.
+              $stmt = $this->pdo->prepare('SELECT MAX(`userID`) FROM `resume`;');
               $edit = $stmt->fetch(PDO::FETCH_ASSOC);
-              $stmt = $this->pdo->prepare("ALTER TABLE `accounts` AUTO_INCREMENT = '$edit';");
+              $stmt = $this->pdo->prepare("ALTER TABLE `resume` AUTO_INCREMENT = '$edit';");
               $stmt = null; $edit = null;
               
-              // Reset auto increment in Contact from the current highest contactID.
-              $stmt = $this->pdo->prepare('SELECT MAX( `contactID` ) FROM `contact`;');
-              $edit = $stmt->fetch(PDO::FETCH_ASSOC);
-              $stmt = $this->pdo->prepare("ALTER TABLE `contact` AUTO_INCREMENT = '$edit';");
-              $stmt = null; $edit = null;
-
               // When removal is completed, erase the session and make a new one.
               session_unset();
               session_destroy();
@@ -69,6 +63,24 @@
         } 
       }
 
-      public function 
-  }
+      public function verifyPassword() {
+        // Select the record to be compared.
+        $stmt = $this->pdo->prepare('SELECT password FROM `resume` WHERE userID = ?');
+
+        // Bind the input parameter to use parameterized queries.
+        $stmt->bindValue(1, $_POST['user_id'], PDO::PARAM_INT);
+        $stmt->execute();
+        $passHash = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!password_verify($_POST['pwd'], $passHash['password'])) {
+            // Passwords don't match!
+            $_SESSION['error'] = "Passwords don't match!";
+                header('Location: ../account.php?');
+                exit();
+            } else {
+                // Password matches, delete the user.
+                $this->deleteUser();
+            }
+        }
+      }
 ?>
