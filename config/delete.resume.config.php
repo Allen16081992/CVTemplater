07841d9a393902381d52f724ce31109db5ab62d0
loubna faress <9008427@student.zadkine.nl>
2023-06-09 12:03:23 +0200

    delete Resume aangepast

--- config/delete.resume.config.php
+++ config/delete.resume.config.php
@@ -1,10 +1,10 @@
 <?php // Loubna Faress
 
-  // use the (improved) database connection.
-  include 'idb.config.php';
+  // Start the session to enable session data use and error messages.
+  session_start();
 
-  // Start a session to for data use, and for displaying error messages.
-  include 'peripherals/session_start.config.php';
+  // Use the (improved) database connection.
+  include 'idb.config.php';
 
   class deleteResume {
     private $pdo;
@@ -14,20 +14,20 @@
         $this->pdo = $database->connect();
     }
 
-    public function deleteResume() {
+    public function deleteResume($resumetitle, $userID) {
       // Verify if the userID exists
-      if (isset($resumetitle,$userID)) {
+      if (isset($resumetitle, $userID)) {
 
-        // Select the record that is to be 'deleteResume'
+        // Select the record that is to be deleted
         $stmt = $this->pdo->prepare('SELECT resumeID FROM `resume` WHERE resumetitle = ? AND userID = ?');
-        $stmt->execute($resumetitle,$userID);
+        $stmt->execute([$resumetitle, $userID]);
         $delete = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if (!$delete) {
             $delete = null;
-            // Success message by sessions instead of url parsing.
+            // Error message by sessions instead of URL parsing.
             $_SESSION['error'] = 'User not found';
-            header('location: ../account.php?');
+            header('location: ../account.php');
             exit();
         }
 
@@ -36,29 +36,27 @@
 
           // Erase data from resume
           $stmt = $this->pdo->prepare('DELETE FROM `resume` WHERE resumeID = ? AND userID = ?');
-          $stmt->execute($delete,$userID);
+          $stmt->execute([$delete['resumeID'], $userID]);
           $stmt = null;
 
-          // Queries voor de andere tabellen zijn niet meer nodig dankzij het volgende.
-          // Buiten accounts, heeft nu elk tabel de foreign key: ON DELETE & UPDATE (van userID) CASCADE.
-
-          // Reset auto increment in resume from the current highest resumeID.
-          $stmt = $this->pdo->prepare('SELECT MAX(`resumeID`) FROM `resume`;');
+          // Reset auto increment in resume from the current highest resumeID
+          $stmt = $this->pdo->prepare('SELECT MAX(`resumeID`) FROM `resume`');
+          $stmt->execute();
           $edit = $stmt->fetch(PDO::FETCH_ASSOC);
-          $stmt = $this->pdo->prepare("ALTER TABLE `resume` AUTO_INCREMENT = '$edit';");
-          $stmt = null; $edit = null;
-          
-          // When removal is completed, erase the session and make a new one for message.
-          session_unset();
-          session_destroy();
+          $stmt = $this->pdo->prepare("ALTER TABLE `resume` AUTO_INCREMENT = :edit");
+          $stmt->bindValue(':edit', $edit['resumeID']);
+          $stmt->execute();
 
-          // Error Messages by session instead of url parsing.
-          session_start();
+          // Success message by session instead of URL parsing
           $_SESSION['success'] = 'User deleted successfully';
-          header('Location: ../index.php?');
+          header('Location: ../index.php');
           exit();
         }
       } 
+    }
+
+    public function __destruct() {
+      $this->pdo = null;
     }
   }
 
@@ -66,7 +64,7 @@
     $resumetitle = $_POST['selectCv'];
     $userID = $_SESSION['user_id'];
     
-    //Create an instance of FetchResumeID and fetch the resume ID
+    // Create an instance of deleteResume and delete the resume
     $deleteResume = new deleteResume();
     $deleteResume->deleteResume($resumetitle, $userID);
   }    
