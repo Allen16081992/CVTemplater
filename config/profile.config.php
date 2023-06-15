bbc62adebc2bbcc03e180fd2bd9f5cc53fddedf6
Allen <leander_lightlord99@hotmail.com>
2023-06-15 23:12:19 +0200

    profile image display error destroyed.

--- config/profile.config.php
+++ config/profile.config.php
@@ -25,90 +25,86 @@
 
         public function setProfileInfo() {
 
-                if (isset($this->userID) && (isset($this->resumeID))) {
-                    // Check if a file was submitted
-                    if ($this->fileUpload['name'] !== '') {
-                        // Generate a unique file name
-                        $filename = uniqid() . '_' . $this->fileUpload['name'];
-                        $filepath = '../img/avatars/' . $filename;
+            if (isset($this->userID) && (isset($this->resumeID))) {
+                // Check if a file was submitted
+                if ($this->fileUpload['name'] !== '') {
+                    // Generate a unique file name
+                    $filename = uniqid() . '_' . $this->fileUpload['name'];
+                    $filepath = '../img/avatars/' . $filename;
 
-                        // Move the uploaded file to the desired location
-                        move_uploaded_file($this->fileUpload['tmp_name'], $filepath);
-                    }
+                    // Move the uploaded file to the desired location
+                    move_uploaded_file($this->fileUpload['tmp_name'], $filepath);
+                }
 
-                    // Select user record
-                    $stmt = $this->pdo->prepare('SELECT * FROM `profile` WHERE resumeID = :resumeID AND userID = :userID');
+                // Select user record
+                $stmt = $this->pdo->prepare('SELECT * FROM `profile` WHERE resumeID = :resumeID AND userID = :userID');
+                $stmt->bindParam(':resumeID', $this->resumeID);
+                $stmt->bindParam(':userID', $this->userID);
+
+                if (!$stmt->execute()) {
+                    $stmt = null;
+                    $_SESSION['error'] = 'Profile database query failed.';
+                    header('location: ../client.php');
+                    exit(); 
+                } 
+
+                if($stmt->rowCount() == 0 ) {
+                    $stmt = null;
+
+                    // SQL statement for all data and file details
+                    $stmt = $this->pdo->prepare(
+                        'INSERT INTO `profile` (profileintro, profiledesc, filePath, fileName, resumeID, userID)
+                            VALUES (:intro, :desc, :filepath, :filename, :resumeID, :userID)'
+                    ); 
+                    $stmt->bindParam(':intro', $this->intro);
+                    $stmt->bindParam(':desc', $this->desc);   
+                    $stmt->bindParam(':filepath', $filepath);
+                    $stmt->bindParam(':filename', $filename);
                     $stmt->bindParam(':resumeID', $this->resumeID);
                     $stmt->bindParam(':userID', $this->userID);
 
                     if (!$stmt->execute()) {
                         $stmt = null;
-                        $_SESSION['error'] = 'Profile database query failed.';
+                        $_SESSION['error'] = 'Unable to insert profile data.';
                         header('location: ../client.php');
                         exit(); 
-                    } 
-
-                    if($stmt->rowCount() == 0 ) {
-                        $stmt = null;
-
-                        // SQL statement for all data and file details
-                        $stmt = $this->pdo->prepare(
-                            'INSERT INTO `profile` (profileintro, profiledesc, filePath, fileName, resumeID, userID)
-                             VALUES (:intro, :desc, :filepath, :filename, :resumeID, :userID)'
-                        ); 
-                        $stmt->bindParam(':intro', $this->intro);
-                        $stmt->bindParam(':desc', $this->desc);   
-                        $stmt->bindParam(':filepath', $filepath);
-                        $stmt->bindParam(':filename', $filename);
-                        $stmt->bindParam(':resumeID', $this->resumeID);
-                        $stmt->bindParam(':userID', $this->userID);
-
-                        if (!$stmt->execute()) {
-                            $stmt = null;
-                            $_SESSION['error'] = 'Unable to insert profile data.';
-                            header('location: ../client.php');
-                            exit(); 
-                        }
-
-                        // Check if something was inserted
-                        if ($stmt->rowCount() > 0) {
-                            // Profile insert successful
-                            $_SESSION['success'] = "Profile info created.";
-                        }
-                    } 
-
-                    if ($stmt->rowCount() > 0) {
-                        $stmt = null;
-
-                        // SQL statement for all data and file details
-                        $stmt = $this->pdo->prepare(
-                            'UPDATE `profile` SET profileintro = :intro, profiledesc = :desc, filePath = :filepath, fileName = :filename 
-                             WHERE resumeID = :resumeID AND userID = :userID'
-                        );    
-                        $stmt->bindParam(':intro', $this->intro);
-                        $stmt->bindParam(':desc', $this->desc); 
-                        $stmt->bindParam(':filepath', $filepath);
-                        $stmt->bindParam(':filename', $filename);
-                        $stmt->bindParam(':resumeID', $this->resumeID);
-                        $stmt->bindParam(':userID', $this->userID);
-
-                        if (!$stmt->execute()) {
-                            $stmt = null;
-                            $_SESSION['error'] = 'Unable to update profile data.';
-                            header('location: ../client.php');
-                            exit(); 
-                        }
-
-                        // display a message
-                        $_SESSION['success'] = "Profile info updated.";
                     }
+
+                    // display a message
+                    $_SESSION['success'] = "Profile info created.";
+                } 
+
+                if ($stmt->rowCount() > 0) {
                     $stmt = null;
 
-                    // Redirect to the appropriate page
-                    header('Location: ../client.php');
-                    exit();
-                }
+                    // SQL statement for all data and file details
+                    $stmt = $this->pdo->prepare(
+                        'UPDATE `profile` SET profileintro = :intro, profiledesc = :desc, filePath = :filepath, fileName = :filename 
+                            WHERE resumeID = :resumeID AND userID = :userID'
+                    );    
+                    $stmt->bindParam(':intro', $this->intro);
+                    $stmt->bindParam(':desc', $this->desc); 
+                    $stmt->bindParam(':filepath', $filepath);
+                    $stmt->bindParam(':filename', $filename);
+                    $stmt->bindParam(':resumeID', $this->resumeID);
+                    $stmt->bindParam(':userID', $this->userID);
 
+                    if (!$stmt->execute()) {
+                        $stmt = null;
+                        $_SESSION['error'] = 'Unable to update profile data.';
+                        header('location: ../client.php');
+                        exit(); 
+                    }
+
+                    // display a message
+                    $_SESSION['success'] = "Profile info updated.";
+                }
+                $stmt = null;
+
+                // Redirect to the appropriate page
+                header('Location: ../client.php');
+                exit();
+            }
         }
     }
 
@@ -121,15 +117,6 @@
         $intro = $_POST['intro'];
         $desc = $_POST['desc'];
         $fileUpload = $_FILES['file-upload'];
-
-        if ($fileUpload['name'] !== '') {
-            // Generate a unique file name
-            $filename = uniqid() . '_' . $fileUpload['name'];
-            $filepath = '../img/avatars/' . $filename;
-
-            // Move the uploaded file to the desired location
-            move_uploaded_file($fileUpload['tmp_name'], $filepath);
-        }
         
         $profile = new Profile($intro, $desc, $fileUpload, $resumeID, $userID);
         $profile->setProfileInfo();
